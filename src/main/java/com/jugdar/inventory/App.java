857901710fb2febdc6d0b37ce8eebd9743ba474d
Kiran Jugdar <kiranjugdar@yahoo.com>
2018-01-21 10:08:32 -0500

    Added instruction to readme

--- src/main/java/com/jugdar/inventory/App.java
+++ src/main/java/com/jugdar/inventory/App.java
@@ -7,26 +7,34 @@ import com.jugdar.inventory.service.helper.CommandInterpreter;
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		CommandInterpreter commandInterpreter = new CommandInterpreter();
+		Scanner scanner = null;
 
-		// create a scanner so we can read the command-line input
-	    Scanner scanner = new Scanner(System.in).useDelimiter("\\n");
-	    
-	    String command = "";
+		try {
+			// create a scanner so we can read the command-line input
+			scanner = new Scanner(System.in).useDelimiter("\\n");
 
-	    while (true) {
-	    	System.out.println("Enter command: ");
-	    	command = scanner.next(); {
-	        if (command.equals("quit") || command.equals("q")) {
-	        	System.out.println("Done. Thank you.");
-	        	break;
-	        }
-	        
-	        commandInterpreter.run(command);
-	       
-	    	}
-	    }
+			String command = "";
+
+			while (true) {
+				System.out.println("Enter command: ");
+				command = scanner.next();
+				{
+					if (command.equals("quit") || command.equals("q")) {
+						System.out.println("Done. Thank you.");
+						break;
+					}
+
+					commandInterpreter.run(command);
+
+				}
+			}
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
 
 	}
 }
